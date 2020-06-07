697f68ad8274e389e9865afdb41feb5fe0cc6bf9
sulkith <sulkith@posteo.de>
2020-06-07 22:34:00 +0200

    added a importer for Atmel Start Code, so it is easy to replace the includen Base Config

---
+++ ATSAML22G18A_LowPowerArm/Software/AtmelStart/AtmelStartIntegrate/importAtzip.sh
@@ -0,0 +1,29 @@
+#mkdir atstart
+#unzip *.atzip -d atstart/
+mkdir atresources
+cp atstart/gcc/Makefile atresources/Makefile_suggestion
+cp atstart/atmel_start_config.atstart atresources
+#cp atstart/main.c main.c
+#cp atstart/driver_init.* . 
+sed -i "s/hpl\//atstart\/hpl\//" atresources/Makefile_suggestion
+sed -i "s/hal\//atstart\/hal\//" atresources/Makefile_suggestion
+sed -i "s/gcc\//atstart\/gcc\//" atresources/Makefile_suggestion
+sed -i "s/CMSIS\//atstart\/CMSIS\//" atresources/Makefile_suggestion
+sed -i "s/gcc /atstart\/gcc /" atresources/Makefile_suggestion
+sed -i "s/examples/atstart\/examples/" atresources/Makefile_suggestion
+sed -i "s/atmel_start.o/atstart\/atmel_start.o/" atresources/Makefile_suggestion
+sed -i "s/atmel_start.d/atstart\/atmel_start.d/" atresources/Makefile_suggestion
+sed -i "s/driver_init.o/atstart\/driver_init.o/" atresources/Makefile_suggestion
+sed -i "s/driver_init.d/atstart\/driver_init.d/" atresources/Makefile_suggestion
+sed -i "s/\.\.\/config/atstart\/config/" atresources/Makefile_suggestion
+sed -i "s/\.\.\/include/atstart\/include/" atresources/Makefile_suggestion
+sed -i "s/\.\.\/hri/atstart\/hri/" atresources/Makefile_suggestion
+sed -i "s/\.\.\/atstart/atstart/" atresources/Makefile_suggestion
+sed -i "s/DIR_INCLUDES +=/DIR_INCLUDES += -I atstart /" atresources/Makefile_suggestion
+sed -i "s/AtmelStart/main/" atresources/Makefile_suggestion
+#echo sed -i "s/vpath/\#vpath/
+#sed -i "s/\.\.\//.\//" atresources/Makefile_suggestion
+
+if [ ! -f Makefile ]; then
+    cp atresources/Makefile_suggestion Makefile
+fi
---
+++ ATSAML22G18A_LowPowerArm/Software/AtmelStart/AtmelStartIntegrate/main.c
@@ -0,0 +1,47 @@
+#include <atmel_start.h>
+
+void delay(int n)
+{
+	int i;
+
+	for (;n >0; n--)
+	{
+		for (i=0;i<100;i++)
+			__asm("nop");
+	}
+}
+void display(uint8_t code)
+{
+	PORT->Group[0].OUTSET.reg = (code<<8)&0xFF00;
+	PORT->Group[0].OUTCLR.reg = (~(code)<<8)&0xFF00;
+
+}
+int main(void)
+{
+	atmel_start_init();
+
+//	PORT->Group[0].DIR.reg = 0xFF00;
+	while(1)
+	{
+		display(0xAA);
+		display(0x0F);
+		for(uint8_t i = 0;i<20;i++)
+		{
+			PORT->Group[0].OUTTGL.reg |= 0xFF00;
+			delay(500);
+		}
+		for(uint8_t i = 0;i<100;i++)
+		{
+			display(i);
+			delay(1000);
+		}
+		for(uint16_t i = 0;i<1000;i++)
+		{
+			display(255);
+			delay(10);
+			display(0);
+			delay(40);
+		}
+
+	}
+}
