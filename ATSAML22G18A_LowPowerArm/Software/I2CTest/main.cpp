d84d221da8a388b7156c14408f4f103f26e3b4b5
sulkith <sulkith@posteo.de>
2020-06-21 20:06:34 +0200

    improved Display output

--- ATSAML22G18A_LowPowerArm/Software/I2CTest/main.cpp
+++ ATSAML22G18A_LowPowerArm/Software/I2CTest/main.cpp
@@ -20,16 +20,26 @@ void display(uint8_t code)
 	PORT->Group[0].OUTSET.reg = (code<<8)&0xFF00;
 	PORT->Group[0].OUTCLR.reg = (~(code)<<8)&0xFF00;
 }
+void callbackI2C(uint8_t len, uint8_t *data)
+{
+
+}
+void read_AM2315()
+{
+	twi_read(0x5C,20,&callbackI2C);
+
+}
 int main(void)
 {
 	atmel_start_init();
+	char helloWorld0[] = "Hello World0!";
+	helloWorld0[11] = 0x32;
 	oled.init();
 	oled.clear();
-	oled.drawString("Hello World!!!",0,0);
-	oled.drawString("Hello World!!!",0,8);
-	oled.drawString("Hello World!!!",0,16);
-	oled.drawString("Hello World!!!",0,24);
-	oled.drawString("Hello World!!!",0,32);
+	oled.drawString(helloWorld0,0,0);
+	oled.drawString("Hello World1!!!",0,8);
+	oled.drawString("Hello World2!!!",0,16);
+	oled.drawString("Hello World3!!!",0,24);
 	oled.show();
 //	PORT->Group[0].DIR.reg = 0xFF00;
 //	i2c_m_sync_get_io_descriptor(&I2C_0, &I2C_0_io);
@@ -41,15 +51,17 @@ int main(void)
 		{
 			PORT->Group[0].OUTTGL.reg |= 0xFF00;
 			delay_ms(100);
-	oled.drawString("Hello World!",0,0);
-	oled.show();
+			helloWorld0[11] = 0x30 + i%10;
+			oled.drawString(helloWorld0,0,0);
+			oled.show();
 		}
 		for(uint8_t i = 0;i<100;i++)
 		{
 			display(i);
 			delay_ms(100);
-	oled.drawString("Hello World!",0,0);
-	oled.show();
+			helloWorld0[11] = 0x30 + i%10;
+			oled.drawString(helloWorld0,0,0);
+			oled.show();
 		}
 		for(uint16_t i = 0;i<1000;i++)
 		{
@@ -57,8 +69,9 @@ int main(void)
 			delay_ms(1);
 			display(0);
 			delay_ms(4);
-	oled.drawString("Hello World!",0,0);
-	oled.show();
+			helloWorld0[11] = 0x30 + i%10;
+			oled.drawString(helloWorld0,0,0);
+			oled.show();
 		}
 
 	}
